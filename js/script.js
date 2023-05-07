00ff7dc323042a8a248e0d27420f499195e1b05c
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-05-07 22:09:30 +0530

    todo view request added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 function todoListLoader() {
   let container = document.getElementById("todoContainer");
-  let todoList = "";
 
-  for (let index = 0; index < 3; index++) {
-    let todo = "eat food" + index;
-    let date = "2023.05.07";
-    let time = "09:46 PM";
+  // send the request
+  let request = new XMLHttpRequest();
 
-    let todoItemUi = `
-  <div class="card primary-box mx-auto mt-5 rounded-4 text-bg-warning" style="width: 600px; height: 7rem">
-    <div class="card-body">
-      <p class="card-text pt-3 pb-0">${todo}</p>
-      <p class="card-text pt-1 pb-0 text-danger">
-        ${date} &nbsp; &nbsp; ${time}
-      </p>
-    </div>
-  </div>
-  `;
+  request.onreadystatechange = function () {
+    if (request.readyState == 4) {
+      let response = request.responseText; //Json
+      let responseArray = JSON.parse(response);
 
-    todoList += todoItemUi;
-  }
+      let todoList = "";
+      for (let index = 0; index < responseArray.length; index++) {
+        let todoItemData = responseArray[index];
 
-  container.innerHTML = todoList;
+        let todo = todoItemData.title;
+        let date = todoItemData.dueDate;
+        let time = todoItemData.dueDate;
+
+        let todoItemUi = `
+      <div class="card primary-box mx-auto mt-5 rounded-4 text-bg-warning" style="width: 600px; height: 7rem">
+        <div class="card-body">
+          <p class="card-text pt-3 pb-0">${todo}</p>
+          <p class="card-text pt-1 pb-0 text-danger">
+            ${date} &nbsp; &nbsp; ${time}
+          </p>
+        </div>
+      </div>
+      `;
+
+        todoList += todoItemUi;
+      }
+
+      container.innerHTML = todoList;
+    }
+  };
+
+  request.open(
+    "GET",
+    "http://localhost/voodooDigital/study/todo-training-malindu/api/todoViewProcess.php",
+    true
+  );
+  request.send();
 }
+
+todoListLoader();
